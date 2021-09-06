b28695ae799264ec04482b6a954bdb549e08e1fd
neekith <84722963+neekith@users.noreply.github.com>
2021-09-06 19:28:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,13 +103,13 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=06 && hour<=19){
+    if(hour>=06 && hour<19){
         bg = "sprites/bg1.png";
     }
     else{
